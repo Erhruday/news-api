749a57ecdcf31ce9f4f45e0ff10830b3834b89f2
Hruday <hrudayranjan8@gmail.com>
2023-02-02 08:19:49 +0530

    feat:db updatation

--- src/components/LeftSideContent/LeftSideContent.jsx
+++ src/components/LeftSideContent/LeftSideContent.jsx
@@ -1,4 +1,10 @@
-import { Button, Pagination, TextField } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  Pagination,
+  TextField,
+} from "@mui/material";
 import React, { useState, useEffect } from "react";
 import "./LeftSideContent.css";
 
@@ -29,6 +35,12 @@ export default function LeftSideContent({
   // const [searchResult, setSearchResult] = useState(data?.hits);
 
   const [pageNumber, setPageNumber] = useState(1);
+
+  const [open, setOpen] = useState(false);
+  const [savedDataFromDB, setsavedDataFromDB] = useState(null);
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const debouncedSearchQuery = useDebounce(searchQuery, 500);
 
@@ -61,22 +73,43 @@ export default function LeftSideContent({
     // console.log(newsId, "news Id");
   };
 
-  // useEffect(() => {
-  //   const filteredResults = data?.hits?.filter((item) =>
-  //     item.title.toLowerCase().includes(searchQuery.toLowerCase())
-  //   );
-  //   setSearchResult(filteredResults);
-  // }, [searchQuery, data?.hits]);
-
   const paginationOnChange = (event, newPage) => {
     setPageNumber(newPage);
     console.log(newPage);
   };
 
   const saveToDBBtnOnClick = () => {
+    let newsData = [];
     data?.hits?.map((elm, i) => {
+      newsData.push({
+        title: elm.title,
+        author: elm.author,
+        created_at: elm.created_at,
+      });
       return console.log(elm.title, elm.author, elm.created_at);
     });
+
+    const payload = JSON.stringify(newsData);
+
+    fetch("https://puffy-temporal-quiet.glitch.me/post-data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: payload,
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  };
+
+  const getDataBtnOnClick = () => {
+    setOpen(true);
+
+    fetch("https://puffy-temporal-quiet.glitch.me/get-data")
+      .then((response) => response.json())
+      .then((data) => setsavedDataFromDB(data))
+      .catch((error) => console.error(error));
   };
 
   return (
@@ -141,6 +174,54 @@ export default function LeftSideContent({
       <Button variant="contained" color="success" onClick={saveToDBBtnOnClick}>
         Save to DB
       </Button>
+
+      <Button
+        sx={{ ml: 3 }}
+        variant="contained"
+        color="success"
+        onClick={getDataBtnOnClick}
+      >
+        Get Data
+      </Button>
+
+      <Dialog
+        maxWidth="lg"
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <div style={{ padding: "10px" }}>
+          <h1>DB Data</h1>
+          <div style={{ minWidth: "600px", minHeight: "200px" }}>
+            {savedDataFromDB?.map((elm, i) => {
+              return (
+                <div
+                  key={i}
+                  style={{
+                    backgroundColor: "rgb(234, 234, 255)",
+                    marginBottom: "10px",
+                    padding: "15px",
+                    borderRadius: "5px",
+                  }}
+                >
+                  <b style={{ color: "red", marginBottom: "10px" }}>
+                    <u>No:{i + 1}</u>
+                  </b>{" "}
+                  <br />
+                  <span>Author : {elm.author}</span> |&nbsp;
+                  <span>Created Date: {elm.created_at}</span>
+                  <h2>Title : {elm.news_title}</h2>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <DialogActions>
+          <Button variant="contained" autoFocus onClick={() => setOpen(false)}>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
 
       <div style={{ margin: "50px 0" }}>
         <Pagination
